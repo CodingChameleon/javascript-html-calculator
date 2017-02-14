5a141a47e633b678a660a7bcd6ab183298321f4e
Naia McNatt <nmcnatt@wlapcs.org>
2017-02-14 21:42:04 +0000

    yay

---
+++ script.js
@@ -0,0 +1,6 @@
+function clearDisplay(){
+    document.getElementById("display-bar").innerHTML = "";
+}
+function display(userInput){
+    document.getElementById("display-bar").innerHTML = userInput;
+}
