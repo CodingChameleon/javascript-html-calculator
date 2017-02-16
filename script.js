4ba966588714eda7380dda80ee07ef4517b58b01
Naia McNatt <nmcnatt@wlapcs.org>
2017-02-16 17:14:23 +0000

    I found the problem

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ function clearDisplay(){
     document.getElementById("display-bar").innerHTML = "";
 }
 function display(userInput){
-    document.getElementById("display-bar").innerHTML = userInput;
+    document.getElementById("display-bar").innerHTML += userInput;
+}
+function calculate(){
+ var answer = eval(document.getElementById("display-bar").innerHTML);
+    document.getElementById("display-bar").innerHTML = answer;
 }
